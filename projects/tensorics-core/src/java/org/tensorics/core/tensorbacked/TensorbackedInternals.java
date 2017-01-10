6086539889d4a4fdef8de96d384871ab5b37cd32
Andrea Calia <andrea.calia.46@gmail.com>
2017-01-10 09:49:17 +0100

    Built from 3be1aac

--- projects/tensorics-core/src/java/org/tensorics/core/tensorbacked/TensorbackedInternals.java
+++ projects/tensorics-core/src/java/org/tensorics/core/tensorbacked/TensorbackedInternals.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.tensorics.core.tensor.Positions;
 import org.tensorics.core.tensor.Shape;
 import org.tensorics.core.tensor.Tensor;
 import org.tensorics.core.tensorbacked.annotation.Dimensions;
@@ -56,7 +57,7 @@ public final class TensorbackedInternals {
      * @return the set of dimentions (classses of coordinates) which are required to create an instance of the given
      *         class.
      */
-    public static Set<Class<?>> dimensionsOf(Class<? extends Tensorbacked<?>> tensorBackedClass) {
+    public static <T extends Tensorbacked<V>, V> Set<Class<?>> dimensionsOf(Class<T> tensorBackedClass) {
         Dimensions dimensionAnnotation = tensorBackedClass.getAnnotation(Dimensions.class);
         if (dimensionAnnotation == null) {
             throw new IllegalArgumentException(
@@ -74,14 +75,14 @@ public final class TensorbackedInternals {
      * @return a new instance of the given class, backed by the given tensor
      */
     public static <V, T extends Tensorbacked<V>> T createBackedByTensor(Class<T> tensorBackedClass, Tensor<V> tensor) {
-        verifyDimensions(tensorBackedClass, tensor);
         return instantiatorFor(tensorBackedClass).ofType(CONSTRUCTOR).withArgumentType(Tensor.class).create(tensor);
     }
 
-    private static <T extends Tensorbacked<V>, V> void verifyDimensions(Class<T> tensorBackedClass, Tensor<?> tensor) {
+    static <T extends Tensorbacked<V>, V> void verifyDimensions(Class<T> tensorBackedClass, Tensor<V> tensor) {
         Set<Class<?>> targetDimensions = dimensionsOf(tensorBackedClass);
         Set<Class<?>> tensorDimensions = tensor.shape().dimensionSet();
-        if (!targetDimensions.equals(tensorDimensions)) {
+        
+        if (!Positions.areDimensionsConsistent(targetDimensions, tensorDimensions)) {
             throw new IllegalArgumentException("Dimensions of target class (" + targetDimensions
                     + ") do not match dimensions of given tensor (" + tensorDimensions + "). Cannot create object.");
         }
